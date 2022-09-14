175cdce97b3fcd8f7f98db15139ea90013091054
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-09-14 21:06:12 +0530

    Update eks-anywhere.rb

--- Formula/eks-anywhere.rb
+++ Formula/eks-anywhere.rb
@@ -4,13 +4,13 @@ homepage "https://github.com/aws/eks-anywhere"
 version "0.10.1"
 
 if OS.mac?
-  url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/18/artifacts/eks-a/v0.10.1/darwin/amd64/eksctl-anywhere-v0.10.1-darwin-amd64.tar.gz"
-  sha256 "c80515cdc3a35ce44adfda7d45a02f571b13ba7a50d1e1c52d34b66f785fe9e7"
+  url "https://github.com/aws/eks-anywhere/releases/download/v0.10.1/eksctl-anywhere-v0.10.1-darwin-amd64.tar.gz"
+  sha256 "c8f2770b6a2785bea4bb14321d3974e4ea7aa9867acbd379c549c73f6ddfc120"
 end
 
 if OS.linux?
-  url "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/18/artifacts/eks-a/v0.10.1/linux/amd64/eksctl-anywhere-v0.10.1-linux-amd64.tar.gz"
-  sha256 "bd5ec0ae6fe5360ddb56ba00552a5544048d4b9d43d3e8ca3ba86982efde8325"
+  url "https://github.com/aws/eks-anywhere/releases/download/v0.10.1/eksctl-anywhere-v0.10.1-linux-amd64.tar.gz"
+  sha256 "a7628f2fa7f3e03b2c3f5b2a4afb0f5b547738ec41e46ba94484cbf2639fd056"
 end
 
 if Hardware::CPU.arm?
